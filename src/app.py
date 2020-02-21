89f4639a42e3533e768315bad72057e120bc310e
lucari <richard@lucari.co.uk>
2020-02-21 13:09:28 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,1 +1,14 @@
+# The Docker image contains the following code
+from flask import Flask
+import os
+import socket
 
+app = Flask(__name__)
+
+@app.route("/")
+def sayHello():
+    html = "<div style='text-align:center;margin:20px;'><h1>Greetings from Richy</h1>This is a simple test page</div>"
+    return html
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=80)
